ea65f164bb0cb185981136b112fbf4c13dd0a186
Cluttered04 <sbpwem@gmail.com>
2019-06-05 10:41:21 -0400

    merging

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿SELECT e.Id AS 'Employee Id', e.FirstName, e.LastName, e.IsSuperVisor, e.DepartmentId,
+                        d.Id AS 'Department Id', d.Name AS 'Department', d.Budget ,c.Id AS 'Computer Id', tp.Name AS 
+						ProgramName,
+						c.Make, c.Manufacturer, c.PurchaseDate, c.DecomissionDate, tp.Id AS 'Training Id'
+                        FROM Employee e LEFT JOIN Department d ON e.DepartmentId = d.Id
+						LEFT JOIN ComputerEmployee ce ON e.Id = ce.EmployeeId
+                        LEFT JOIN Computer c ON ce.ComputerId=c.Id LEFT JOIN EmployeeTraining et ON e.Id = et.EmployeeId 
+						LEFT JOIN 
+						TrainingProgram tp 
+						ON et.TrainingProgramId = tp.Id WHERE e.Id = 4
