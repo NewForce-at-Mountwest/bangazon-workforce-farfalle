2910cd6e23ed0006a196c01f5394cf2e97e03410
Connor FitzGerald <connorreecefitzgerald@gmail.com>
2019-06-05 10:22:13 -0400

    added some display names to trainings

--- BangazonWorkforce/Models/TrainingProgram.cs
+++ BangazonWorkforce/Models/TrainingProgram.cs
@@ -11,10 +11,13 @@ namespace BangazonWorkforce.Models
     {
 
         public int Id { get; set; }
-
+        [Display(Name = "Training Name")]
         public string Name { get; set; }
+        [Display(Name = "Start Date")]
         public DateTime StartDate { get; set; }
+        [Display(Name = "End Date")]
         public DateTime EndDate { get; set; }
+        [Display(Name = "Max Attendees")]
         public int MaxAttendees { get; set; }
         public List<Employee> Employees { get; set; } = new List<Employee>();
 
