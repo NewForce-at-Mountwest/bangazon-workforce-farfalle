8d0ec7b4c18196ab730ebde83850bcbd86f273d0
sydneywait <sydneywait@gmail.com>
2019-06-06 13:11:37 -0400

    list is filtering out those that are already assigned

--- BangazonWorkforce/Models/ViewModels/AssignEmployeeViewModel.cs
+++ BangazonWorkforce/Models/ViewModels/AssignEmployeeViewModel.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 
 namespace BangazonWorkforce.Models.ViewModels
 {
-    public class AssignEmployeeViewModel
+    public class AssignEmployeeViewModel 
     {
         public Employee employee { get; set; }
         public int selectedTrainingProgramId { get; set; }
-        private HashSet<SimpleTraining> ThisEmployeeTrainingPrograms { get; set; } = new HashSet<SimpleTraining>();
-        private HashSet<SimpleTraining> AllFutureAvailableTrainingPrograms { get; set; } = new HashSet<SimpleTraining>();
+        private HashSet<SimpleTraining> ThisEmployeeTrainingPrograms { get; set; } = new HashSet<SimpleTraining>(new TrainingComparer());
+        private HashSet<SimpleTraining> AllFutureAvailableTrainingPrograms { get; set; } = new HashSet<SimpleTraining>(new TrainingComparer());
 
         public List<SelectListItem> TrainingPrograms { get; set; }
 
@@ -73,7 +73,7 @@ namespace BangazonWorkforce.Models.ViewModels
                     cmd.CommandText = "SELECT sq.TrainingProgramId AS 'Training Program Id', sq.[training program name] AS 'Training Program Name', sq.[employee count] as 'Employee Count', sq.MaxAttendees AS 'Max Attendees', sq.startDate as 'Start Date' FROM(select tp.id as 'trainingProgramId', tp.[Name] as 'training program name', COUNT(employeeId) as 'employee count', MaxAttendees, startDate FROM EmployeeTraining et FULL JOIN TrainingProgram tp ON et.TrainingProgramId = tp.Id GROUP BY tp.Id, tp. [Name], MaxAttendees, startDate) sq WHERE sq.[employee count] <= sq.MaxAttendees AND sq.StartDate > CURRENT_TIMESTAMP";
                     SqlDataReader reader = cmd.ExecuteReader();
 
-                    HashSet<SimpleTraining> TrainingPrograms = new HashSet<SimpleTraining>();
+                    HashSet<SimpleTraining> TrainingPrograms = new HashSet<SimpleTraining>(new TrainingComparer());
                     while (reader.Read())
                     {
                         TrainingPrograms.Add(new SimpleTraining
@@ -102,7 +102,7 @@ namespace BangazonWorkforce.Models.ViewModels
                     cmd.CommandText = $"SELECT tp.id, tp.name FROM EmployeeTraining et JOIN TrainingProgram tp ON tp.id = et.TrainingProgramId WHERE employeeId={id}";
                     SqlDataReader reader = cmd.ExecuteReader();
 
-                    HashSet<SimpleTraining> ThisEmployeeTrainingPrograms = new HashSet<SimpleTraining>();
+                    HashSet<SimpleTraining> ThisEmployeeTrainingPrograms = new HashSet<SimpleTraining>(new TrainingComparer());
 
 
                     while (reader.Read())
@@ -123,6 +123,19 @@ namespace BangazonWorkforce.Models.ViewModels
                     return ThisEmployeeTrainingPrograms;
                 }
             }
+        }
+    }
+
+    public class TrainingComparer : IEqualityComparer<SimpleTraining>
+    {
+        public bool Equals(SimpleTraining x, SimpleTraining y)
+        {
+            return x.Id==y.Id;
+        }
+
+        public int GetHashCode(SimpleTraining obj)
+        {
+            return obj.Id.GetHashCode();
         }
     }
 }
